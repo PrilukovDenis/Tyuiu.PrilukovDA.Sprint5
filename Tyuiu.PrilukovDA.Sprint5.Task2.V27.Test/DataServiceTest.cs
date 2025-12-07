348c7e95bf9af8fb5f36542dd009dc03a9562ee8
deusvela <d.prilukov@aerokod.ru>
2025-12-07 17:17:32 +0500

    Task2

---
+++ Tyuiu.PrilukovDA.Sprint5.Task2.V27.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.PrilukovDA.Sprint5.Task2.V27.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint5.Task2.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] matrix =
+            {
+                { 1, 4, 3 }, { 1, 1, 4 }, { 4, 3, 8 }
+            };
+            string path = ds.SaveToFileTextData(matrix);
+
+            FileInfo fileInfo = new FileInfo(path);
+            Assert.AreEqual(true, fileInfo.Exists);
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint5.Task2.V27/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.PrilukovDA.Sprint5.Task2.V27.Lib;
+
+DataService ds = new DataService();
+int[,] matrix =
+{
+    { 1, 4, 3 }, { 1, 1, 4 }, { 4, 3, 8 }
+};
+
+string res = ds.SaveToFileTextData(matrix);
+
+Console.WriteLine("Файл создан: " + res);
+
+Console.WriteLine("Содержимое: " + File.ReadAllText(res));
