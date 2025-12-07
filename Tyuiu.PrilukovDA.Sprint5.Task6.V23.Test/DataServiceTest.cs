c7c595247bc8c34862cb2b04801fb98e9e36eeea
deusvela <d.prilukov@aerokod.ru>
2025-12-07 23:02:23 +0500

    Task6

---
+++ Tyuiu.PrilukovDA.Sprint5.Task6.V23.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.PrilukovDA.Sprint5.Task6.V23.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint5.Task6.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            string path = @"C:\Users\prilu\source\repos\Tyuiu.PrilukovDA.Sprint5\Tyuiu.PrilukovDA.Sprint5.Task6.V23\bin\Debug\net8.0\DataSprint5\InPutDataFileTask6V23.txt";
+
+            int res = ds.LoadFromDataFile(path);
+
+            Assert.AreEqual(7, res);
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint5.Task6.V23/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.PrilukovDA.Sprint5.Task6.V23.Lib;
+
+DataService ds = new DataService();
+
+string path = $@"{Directory.GetCurrentDirectory()}InPutDataFileTask6V23.txt";
+
+
+double result = ds.LoadFromDataFile(path);
+Console.WriteLine("Результат: " + result);
